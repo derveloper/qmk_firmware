b4b8bf32c5b7519c6fcde6ec2b5652cd8f6a4742
Tristan Leo <tleo@sipgate.de>
2022-08-17 13:28:05 +0200

    add mac layer

--- keyboards/preonic/keymaps/derveloper/keymap.c
+++ keyboards/preonic/keymaps/derveloper/keymap.c
@@ -27,6 +27,7 @@ enum preonic_layers {
     _LAYER1,
     _LAYER2,
     _LAYER3,
+    _LAYER4,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,9 +60,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_SLEP, KC_WAKE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       BL_TOGG, BL_STEP, BL_BRTG, KC_MUTE,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SGUI(KC_PSCR), TMUXBROADCAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TMUXVSPLIT,    TMUXHSPLIT,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TMUXVSPLIT,    TMUXHSPLIT,    KC_TRNS, KC_TRNS, KC_TRNS, LT(_LAYER3),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, LT(_LAYER4),
         ),
+    
+    [_LAYER4] = LAYOUT_preonic_grid(
+        KC_ESC,         KC_1,         KC_2,         KC_3,    KC_4,             KC_5,   KC_6,   KC_7,         KC_8,         KC_9,         KC_0,            KC_BSPC,
+        KC_DEL,         KC_Q,         KC_W,         KC_E,    KC_R,             KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,            KC_EQL,
+        KC_TAB,         LALT_T(KC_A), LSFT_T(KC_S), KC_D,    LT(_LAYER2,KC_F), KC_G,   KC_H,   LGUI_T(KC_J), RALT_T(KC_K), RSFT_T(KC_L), LALT_T(KC_SCLN), KC_QUOT,
+        KC_LSFT,        KC_Z,         KC_X,         KC_C,    KC_V,             KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_ENT,
+        LGUI_T(KC_GRV), KC_ENT,       KC_LALT,      KC_LCTL, MO(_LAYER1),      KC_SPC, KC_SPC, KC_ENT,       KC_BSPC,      MO(_LAYER3),  KC_RGUI,         MO(_LAYER1)
+        )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
